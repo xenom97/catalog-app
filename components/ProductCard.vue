aa9e02faa68afa1091b9bf02353cadefc0f1e1ae
ahmad <purnomo.ahmad97@gmail.com>
2021-11-19 23:08:25 +0700

    feat: add product card component

---
+++ components/ProductCard.vue
@@ -0,0 +1,88 @@
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api';
+import { IProduct } from '~/entities/product.entity';
+
+export default defineComponent({
+  name: 'ProductCard',
+
+  props: {
+    product: {
+      type: Object as PropType<IProduct>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="card__image">
+      <span v-if="!product.image"> Not Available </span>
+      <img :src="product.image" :alt="product.title" />
+    </div>
+
+    <div class="card__body">
+      <p class="product__title">{{ product.title }}</p>
+      <p class="product__price">$ {{ product.price }}</p>
+
+      <div v-if="product.rating" class="product__rating">
+        <span>⭐{{ product.rating.rate }}</span>
+        <span>({{ product.rating.count }})</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  border: 2px solid #ebebeb;
+  border-radius: 0.75em;
+  transition: 0.2s;
+  background: #fff;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: $default-shadow;
+  }
+
+  &__image {
+    height: 150px;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.75em 0.75em 0 0;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+
+  &__body {
+    padding: 0.75em;
+  }
+}
+
+.product {
+  &__title {
+    font-size: 12px;
+  }
+
+  &__price {
+    font-size: 14px;
+    color: $secondary-color;
+    font-weight: bold;
+    padding: 0.5em 0;
+  }
+
+  &__rating {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    color: #999;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="container">
-    <div v-for="(product, idx) in products" :key="idx" class="card">
-      <div class="card__image">
-        <span v-if="!product.image"> Not Available </span>
-        <img :src="product.image" :alt="product.title" />
-      </div>
-
-      <div class="card__body">
-        <p class="product__title">{{ product.title }}</p>
-        <p class="product__price">$ {{ product.price }}</p>
-
-        <div v-if="product.rating" class="product__rating">
-          <span>⭐{{ product.rating.rate }}</span>
-          <span>({{ product.rating.count }})</span>
-        </div>
-      </div>
-    </div>
+    <ProductCard
+      v-for="(product, idx) in products"
+      :key="idx"
+      :product="product"
+    />
   </div>
 </template>
 
@@ -26,9 +15,14 @@ import {
   useContext,
   watch,
 } from '@nuxtjs/composition-api';
+import ProductCard from '~/components/ProductCard.vue';
 
 export default defineComponent({
   name: 'HomePage',
+
+  components: {
+    ProductCard,
+  },
 
   setup() {
     const { $accessor } = useContext();
@@ -62,57 +56,5 @@ export default defineComponent({
   grid-column-gap: 1em;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   padding: 1em;
-}
-
-.card {
-  border: 2px solid #ebebeb;
-  border-radius: 0.75em;
-  transition: 0.2s;
-  background: #fff;
-
-  &:hover {
-    cursor: pointer;
-    box-shadow: $default-shadow;
-  }
-
-  &__image {
-    height: 150px;
-    background: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 0.75em 0.75em 0 0;
-    overflow: hidden;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
-  }
-
-  &__body {
-    padding: 0.75em;
-  }
-}
-
-.product {
-  &__title {
-    font-size: 12px;
-  }
-
-  &__price {
-    font-size: 14px;
-    color: $secondary-color;
-    font-weight: bold;
-    padding: 0.5em 0;
-  }
-
-  &__rating {
-    display: flex;
-    align-items: center;
-    font-size: 12px;
-    color: #999;
-  }
 }
 </style>
